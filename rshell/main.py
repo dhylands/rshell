5f05d8b870ea91d02d33937701ac629c02dea401
Dave Hylands <dhylands@gmail.com>
2017-04-30 21:17:56 -0700

    Fix problem with b key not working on OSX

--- rshell/main.py
+++ rshell/main.py
@@ -64,11 +64,12 @@ FAKE_INPUT_PROMPT = False
 
 import readline
 import rlcompleter
-if sys.platform == 'darwin':
+if 'libedit' in readline.__doc__:
     readline.parse_and_bind ("bind ^I rl_complete")
     BROKEN_READLINE = True
 else:
     readline.parse_and_bind("tab: complete")
+
 MONTH = ('', 'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
          'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec')
 

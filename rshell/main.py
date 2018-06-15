dhylands/rshell

commit 75695463438044c38ebdb32347ead80e4194e592

Increase robustness of file transfers to remote (#48)
If the remote is slow initializing to receive a file, the first
buffer sent from host to remote may overflow the UART buffer.
To fix this, the "ack" mechanism was moved to the beginning of
the loop so the first buffer write waits for the remote.

In debugging this, it was observed that the dev.read() of the ack
would not timeout causing an indefinite "hang". A timeout was
added to send_file_to_remote() to catch issues on the remote.
The timeout methods were changed to getter/setter properties to
enable (re)setting the timeout to None which indicates block
forever on a serial read.

rshell/main.py
@@ -906,14 +906,16 @@ def recv_file_from_host(src_file, dst_filename, filesize, dst_mode='wb'):
             write_buf = bytearray(buf_size)
             read_buf = bytearray(buf_size)
             while bytes_remaining > 0:
+                # Send back an ack as a form of flow control
+                sys.stdout.write('\x06')
                 read_size = min(bytes_remaining, buf_size)
                 buf_remaining = read_size
                 buf_index = 0
                 while buf_remaining > 0:
                     if HAS_BUFFER:
-                        bytes_read = sys.stdin.buffer.readinto(read_buf, bytes_remaining)
+                        bytes_read = sys.stdin.buffer.readinto(read_buf, read_size)
                     else:
-                        bytes_read = sys.stdin.readinto(read_buf, bytes_remaining)
+                        bytes_read = sys.stdin.readinto(read_buf, read_size)
                     if bytes_read > 0:
                         write_buf[buf_index:bytes_read] = read_buf[0:bytes_read]
                         buf_index += bytes_read
@@ -922,8 +924,6 @@ def recv_file_from_host(src_file, dst_filename, filesize, dst_mode='wb'):
                     dst_file.write(write_buf[0:read_size])
                 else:
                     dst_file.write(ubinascii.unhexlify(write_buf[0:read_size]))
-                # Send back an ack as a form of flow control
-                sys.stdout.write('\x06')
                 bytes_remaining -= read_size
         return True
     except:
@@ -935,7 +935,15 @@ def send_file_to_remote(dev, src_file, dst_filename, filesize, dst_mode='wb'):
        Matches up with recv_file_from_host.
     """
     bytes_remaining = filesize
+    save_timeout = dev.timeout
+    dev.timeout = 1
     while bytes_remaining > 0:
+        # Wait for ack so we don't get too far ahead of the remote
+        ack = dev.read(1)
+        if ack is None or ack != b'\x06':
+            sys.stderr.write("timed out or error in transfer to remote\n")
+            sys.exit(2)
+
         if HAS_BUFFER:
             buf_size = BUFFER_SIZE
         else:
@@ -948,15 +956,9 @@ def send_file_to_remote(dev, src_file, dst_filename, filesize, dst_mode='wb'):
             dev.write(buf)
         else:
             dev.write(binascii.hexlify(buf))
-        # Wait for ack so we don't get too far ahead of the remote
-        while True:
-            char = dev.read(1)
-            if char == b'\x06':
-                break
-            # This should only happen if an error occurs
-            sys.stdout.write(chr(ord(char)))
         bytes_remaining -= read_size
     #sys.stdout.write('\r')
+    dev.timeout = save_timeout
 
 
 def recv_file_from_remote(dev, src_filename, dst_file, filesize):
@@ -1465,13 +1467,18 @@ class DeviceSerial(Device):
     def is_serial_port(self, port):
         return self.dev_name_short == port
 
-    def timeout(self, timeout=None):
+    @property
+    def timeout(self):
+        """Gets the timeout associated with the serial port."""
+        self.check_pyb()
+        return self.pyb.serial.timeout
+
+    @timeout.setter
+    def timeout(self, value):
         """Sets the timeout associated with the serial port."""
         self.check_pyb()
-        if timeout is None:
-            return self.pyb.serial.timeout
         try:
-            self.pyb.serial.timeout = timeout
+            self.pyb.serial.timeout = value
         except:
             # timeout is a property so it calls code, and that can fail
             # if the serial port is closed.
@@ -1495,9 +1502,16 @@ class DeviceNet(Device):
     def default_board_name(self):
         return 'wipy'
 
-    def timeout(self, timeout=None):
+    @property
+    def timeout(self):
         """There is no equivalent to timeout for the telnet connection."""
         return None
+
+    @timeout.setter
+    def timeout(self, value):
+        """There is no equivalent to timeout for the telnet connection."""
+        pass
+
 
 class AutoBool(object):
     """A simple class which allows a boolean to be set to False in conjunction

commit 190d5fc17b26e66a85a9943be40ffc820a1f37db

Fix bug introduced in #48
Bumped version to 0.0.14

rshell/main.py
@@ -2239,12 +2239,13 @@ class Shell(cmd.Cmd):
         """Runs as a thread which has a sole purpose of readding bytes from
            the seril port and writing them to stdout. Used by do_repl.
         """
+        print('repl_serial_to_stdout dev =', dev)
         with self.serial_reader_running:
             try:
-                save_timeout = dev.timeout()
+                save_timeout = dev.timeout
                 # Set a timeout so that the read returns periodically with no data
                 # and allows us to check whether the main thread wants us to quit.
-                dev.timeout(1)
+                dev.timeout = 1
                 while not self.quit_serial_reader:
                     try:
                         char = dev.read(1)
@@ -2269,7 +2270,7 @@ class Shell(cmd.Cmd):
                         continue
                     self.stdout.write(char)
                     self.stdout.flush()
-                dev.timeout(save_timeout)
+                dev.timeout = save_timeout
             except DeviceError:
                 # The device is no longer present.
                 return

rshell/version.py
@@ -1,1 +1,1 @@
-__version__ = '0.0.13'
+__version__ = '0.0.14'
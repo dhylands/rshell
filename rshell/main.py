a056e91ea575b3fd797f4d754a2b3a7c28ae9a35
Lorenzo Gonzales <bizdek@gmail.com>
2020-07-12 10:45:05 +0200

    Added experimental ble-nus-repl support. NRF port needs to be compiled with MICROPY_VFS_FAT=1.

--- rshell/main.py
+++ rshell/main.py
@@ -1329,6 +1329,15 @@ def connect_telnet(name, ip_address=None, user='micro', password='python'):
     add_device(dev)
 
 
+def connect_ble(mac):
+    if not QUIET:
+        if mac == "ble":
+            print('Connecting ble...')
+        else:
+            print('Connecting ble to %s...' % (mac))
+    dev = DeviceBle(mac)
+    add_device(dev)
+
 def connect_serial(port, baud=115200, wait=0):
     """Connect to a MicroPython board via a serial port."""
     if not QUIET:
@@ -1676,6 +1685,32 @@ class DeviceNet(Device):
     @timeout.setter
     def timeout(self, value):
         """There is no equivalent to timeout for the telnet connection."""
+        pass
+
+
+class DeviceBle(Device):
+
+    def __init__(self, mac):
+        self.dev_name_short = 'mpus ({})'.format(mac, "" if mac == "ble" else mac)
+        self.dev_name_long = self.dev_name_short
+
+        try:
+            pyb = Pyboard(mac)
+        except KeyboardInterrupt:
+            raise DeviceError('Interrupted')
+        Device.__init__(self, pyb)
+
+    def default_board_name(self):
+        return 'mpus'
+
+    @property
+    def timeout(self):
+        """Not implemented for the ble connection."""
+        return None
+
+    @timeout.setter
+    def timeout(self, value):
+        """Not implemented for the ble connection."""
         pass
 
 
@@ -2099,6 +2134,7 @@ class Shell(cmd.Cmd):
         """connect TYPE TYPE_PARAMS
            connect serial port [baud]
            connect telnet ip-address-or-name
+           connect ble [mac-addr]
 
            Connects a pyboard to rshell.
         """
@@ -2128,6 +2164,12 @@ class Shell(cmd.Cmd):
                 return
             name = args[1]
             connect_telnet(name)
+        elif connect_type == "ble":
+            if num_args < 2:
+                mac = "ble"
+            else:
+                mac = args[1]
+            connect_ble(mac)
         else:
             print_err('Unrecognized connection TYPE: {}'.format(connect_type))
 

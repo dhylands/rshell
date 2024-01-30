95a5a97364fbae51abcf85c970f8a9d6e1448b2b
n4n5 <its.just.n4n5@gmail.com>
2024-01-30 11:24:19 +0100

    feat: check dir before listing

--- rshell/main.py
+++ rshell/main.py
@@ -830,6 +830,8 @@ def listdir_matches(match):
             # This can happen when a symlink points to a non-existant file.
             pass
         return filename
+    if not os.path.isdir(dirname):
+        return []
     matches = [add_suffix_if_dir(result_prefix + filename)
                for filename in os.listdir(dirname) if filename.startswith(match_prefix)]
     return matches
